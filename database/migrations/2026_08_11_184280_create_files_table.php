3d00ee1d0b1ab177e82ddf2c8e93a7c76e857c58
RamsesSzobotka <vlastoskr@gmail.com>
2026-08-24 15:10:00 -0500

    chore: eliminar comentarios de migracion create_files

--- database/migrations/2026_08_11_184280_create_files_table.php
+++ database/migrations/2026_08_11_184280_create_files_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('files', function (Blueprint $table) {
@@ -32,9 +29,7 @@ return new class extends Migration
             $table->index("hash");
         });
     }
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('files');
